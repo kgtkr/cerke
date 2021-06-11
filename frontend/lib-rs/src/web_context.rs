c3ab7aca439386a2591962f6c033c79aa958efab
kgtkr <kgtkr.jp@gmail.com>
2021-06-12 03:22:46 +0900

    オーディオ再生改善

--- frontend/lib-rs/src/web_context.rs
+++ frontend/lib-rs/src/web_context.rs
@@ -1,33 +1,40 @@
 use crate::context::Context;
 use async_trait::async_trait;
-use js_sys::Promise;
+use js_sys::{ArrayBuffer, Promise};
 use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::JsFuture;
-use web_sys::{CanvasRenderingContext2d, HtmlAudioElement, HtmlImageElement};
+use web_sys::{
+    AudioBuffer, AudioContext, CanvasRenderingContext2d, HtmlAudioElement, HtmlImageElement,
+    Response,
+};
 
 pub struct WebContext {
     canvas_ctx: CanvasRenderingContext2d,
+    audio_ctx: AudioContext,
 }
 
 #[async_trait(?Send)]
 impl Context for WebContext {
     type Image = HtmlImageElement;
-    type Audio = HtmlAudioElement;
+    type Audio = AudioBuffer;
     type Error = JsValue;
 
     async fn load_audio(&mut self, src: &str) -> Result<Self::Audio, Self::Error> {
-        let result = HtmlAudioElement::new().unwrap();
-        let promise = Promise::new(&mut |resolve, reject| {
-            result
-                .add_event_listener_with_callback("loadeddata", &resolve)
-                .unwrap();
-            result
-                .add_event_listener_with_callback("error", &reject)
-                .unwrap();
-        });
-        result.set_src(src);
-        JsFuture::from(promise).await?;
-        Ok(result)
+        let window = web_sys::window().unwrap();
+        let res = JsFuture::from(window.fetch_with_str(src))
+            .await?
+            .dyn_into::<Response>()
+            .unwrap();
+        let buffer = JsFuture::from(res.array_buffer().unwrap())
+            .await?
+            .dyn_into::<ArrayBuffer>()
+            .unwrap();
+        let audio_buffer = JsFuture::from(self.audio_ctx.decode_audio_data(&buffer).unwrap())
+            .await?
+            .dyn_into::<AudioBuffer>()
+            .unwrap();
+
+        Ok(audio_buffer)
     }
 
     async fn load_image(&mut self, src: &str) -> Result<Self::Image, Self::Error> {
@@ -46,13 +53,12 @@ impl Context for WebContext {
     }
 
     fn audio_play(&mut self, audio: &mut Self::Audio) {
-        let _ = audio
-            .clone_node()
-            .unwrap()
-            .dyn_into::<HtmlAudioElement>()
-            .unwrap()
-            .play()
+        let source = self.audio_ctx.create_buffer_source().unwrap();
+        source.set_buffer(Some(audio));
+        source
+            .connect_with_audio_node(&self.audio_ctx.destination())
             .unwrap();
+        source.start().unwrap();
     }
 
     fn draw_image(&mut self, image: &mut Self::Image, x: f64, y: f64, w: f64, h: f64) {
